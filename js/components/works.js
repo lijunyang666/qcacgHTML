881422d99f91528418838b0e1f859c1777ee7987
lijunyang <275120399@qq.com>
2016-11-12 17:31:16 +0800

    g

--- js/components/works.js
+++ js/components/works.js
@@ -89,7 +89,7 @@ var works = Vue.extend({
     
      SZXJ.http('post', PathList.saveOrUpdateBook, _data, 
             (response) => {
-              location.href = '/view/user_info.html#!/bookBlockList';
+              location.href = PathList.TemprootPath + '/view/user_info.html#!/bookBlockList';
             });
     },
     //============================================
