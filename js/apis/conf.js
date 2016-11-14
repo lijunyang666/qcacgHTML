46b32e05e64af59f1dc4ad29e576882dae3090f2
aicheng <617179368@qq.com>
2016-11-14 16:59:09 +0800

    接口

--- js/apis/conf.js
+++ js/apis/conf.js
@@ -1,9 +1,9 @@
 ﻿var PathList = {};
 
 PathList.TemprootPath = '';
-
 PathList.TemprootPath = 'http://127.0.0.1:8020/qcacgHTML';
 
+PathList.rootPath = '/Controller';
 PathList.rootPath = 'http://www.qcacg.cn/Controller';
 
 
@@ -18,17 +18,21 @@ PathList.register = PathList.rootPath + '/register.shtml';
 
 // 查看用户创建作品列表 不需要传数据
 PathList.findBookByUser = PathList.rootPath + '/book/findBookByUser.shtml';
-// 查看用户创建作品目录页 需要传入作品id
+// 查看作者创建作品目录页 需要传入作品id
 PathList.queryBook = PathList.rootPath + '/book/queryBook.shtml'
-// 用户作品新增卷
+// 作者作品新增卷
 PathList.saveOrUpdateVolume = PathList.rootPath + '/volume/saveOrUpdateVolume.shtml';
-// 用户新建作品上传封面页
+// 作者新建作品上传封面页
 PathList.bookUpload = PathList.rootPath + '/book/upload.shtml';
-// 获取分类的接口
-PathList.queryBookType = PathList.rootPath + '/bookType/queryBookType.shtml';
-// 新建与修改作品的接口
+// 作者新建与修改作品的接口
 PathList.saveOrUpdateBook = PathList.rootPath + '/book/saveOrUpdateBook.shtml';
-// 读者阅读章节接口
+// 作者阅读章节接口
 PathList.findContent = PathList.rootPath + '/content/findContent.shtml';
-// 新建章节接口
+// 作者新建章节接口
 PathList.saveOrUpdateContent = PathList.rootPath + '/content/saveOrUpdateContent.shtml';
+
+// 获取作品分类的接口
+PathList.queryBookType = PathList.rootPath + '/bookType/queryBookType.shtml';
+
+// 读者访问目录页
+PathList.queryBookDirectory = PathList.rootPath + '/catalog/queryBookDirectory.shtml';
--- js/apis/vueHttp.js
+++ js/apis/vueHttp.js
@@ -2,16 +2,6 @@ var SZXJ = {};
 SZXJ.http = (_method, _url, _data, successfn, errorfn) => {
   Vue.http.headers.common['JSESSIONID'] = localStorage.getItem('JSESSIONID');
   _URL = _url;
-  // 判断是否是本地调试
-  if (document.domain === '127.0.0.1' || document.domain === 'localhost') {
-    if (_method !== 'pldelete') {
-      // _URL = _url.concat('?x=renren2016whosyourdaddy');
-    }
-  } else {
-    if (_method === 'pldelete') {
-      _URL = _url.substring(0, _url.lastIndexOf('x='));
-    }
-  }
   if (_method === 'get') {
     // _URL
     Vue.http.get(_URL, _data).then(
@@ -43,60 +33,8 @@ SZXJ.http = (_method, _url, _data, successfn, errorfn) => {
       }
     );
   }
-  // delete
-  if (_method === 'delete') {
-    Vue.http.delete(_URL, _data).then(
-      (response) => {
-        console.log('200:');
-        successfn(response);
-      },
-      (err) => {
-        console.log('500:'.concat(err.data.code).concat(':').concat(err.data.msg));
-        console.log(err.data.code.toString().substring(0, 1));
-        if (err.data.code.toString().substring(0, 1) === '8') { // 登陆信息错误 请尝试重新登陆
-          localStorage.removeItem('indexLogin');
-          location.href = '/';
-        }
-        errorfn(err);
-      }
-    );
-  }
-  if (_method === 'pldelete') {
-    Vue.http.delete(_URL, _data).then(
-      (response) => {
-        console.log('200:');
-        successfn(response);
-      },
-      (err) => {
-        console.log('500:'.concat(err.data.code).concat(':').concat(err.data.msg));
-        console.log(err.data.code.toString().substring(0, 1));
-        if (err.data.code.toString().substring(0, 1) === '8') { // 登陆信息错误 请尝试重新登陆
-          localStorage.removeItem('indexLogin');
-          location.href = '/';
-        }
-        errorfn(err);
-      }
-    );
-  }
-  // put
-  if (_method === 'put') {
-    Vue.http.put(_URL, _data).then(
-      (response) => {
-        console.log('200:');
-        successfn(response);
-      },
-      (err) => {
-        console.log('500:'.concat(err.data.code).concat(':').concat(err.data.msg));
-        console.log(err.data.code.toString().substring(0, 1));
-        if (err.data.code.toString().substring(0, 1) === '8') { // 登陆信息错误 请尝试重新登陆
-          localStorage.removeItem('indexLogin');
-          location.href = '/';
-        }
-        errorfn(err);
-      }
-    );
-  }
+  // 图片上传
   if (_method === 'upload') {
-    post(_url, _data, successfn, errorfn);
+     // 
   }
 };
