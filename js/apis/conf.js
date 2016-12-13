9777da4c60759e0d1b66aebaf56ce08a07cc120d
aicheng <617179368@qq.com>
2016-12-13 11:00:22 +0800

    a

--- js/apis/conf.js
+++ js/apis/conf.js
@@ -1,12 +1,12 @@
 ﻿var PathList = {};
 
 
-//PathList.TemprootPath = '';
-PathList.TemprootPath = 'http://127.0.0.1:8020/ROOT'; // 页面跳转用这个
+PathList.TemprootPath = '';
+//PathList.TemprootPath = 'http://127.0.0.1:8020/ROOT'; // 页面跳转用这个
 
-//PathList.rootPath = '/Controller';
+PathList.rootPath = '/Controller';
 //PathList.rootPath = 'http://www.qcacg.cn/Controller';
-PathList.rootPath = 'http://121.196.194.211:8080/Controller';
+//PathList.rootPath = 'http://121.196.194.211:8080/Controller';
 // PathList.rootPath = 'http://192.168.126.25:8080/';
 
 // 登陆的3个请求
